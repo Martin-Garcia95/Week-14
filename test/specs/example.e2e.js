f1efe1dab2f46e86319762c61caac7ec3cbf9e30
Martin Garcia <martin-garcia95@hotmail.com>
2022-11-18 14:03:55 -0300

    TG-14: Locked user test

--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -64,4 +64,28 @@ describe('My Login application', () => {
           await LoginPage.logoutButton.click();
           await browser.refresh()
       });
+
+      it('Locked user, empty password', async () =>{
+          await LoginPage.login('locked_out_user','');
+          await LoginPage.errorMessage.waitForDisplayed();
+          await expect(LoginPage.errorMessage).toBeDisplayed();
+          await expect(LoginPage.errorMessage).toHaveText('Epic sadface: Password is required');
+          await browser.refresh()
+      });
+
+      it('Locked user, invalid password', async () =>{
+          await LoginPage.login('locked_out_user','pepe');
+          await LoginPage.errorMessage.waitForDisplayed();
+          await expect(LoginPage.errorMessage).toBeDisplayed();
+          await expect(LoginPage.errorMessage).toHaveText('Epic sadface: Username and password do not match any user in this service');
+          await browser.refresh()
+      });
+
+      it('Locked user, valid password', async () =>{
+          await LoginPage.login('locked_out_user','secret_sauce');
+          await LoginPage.errorMessage.waitForDisplayed();
+          await expect(LoginPage.errorMessage).toBeDisplayed();
+          await expect(LoginPage.errorMessage).toHaveText('Epic sadface: Sorry, this user has been locked out.');
+          await browser.refresh()
+      });
 })
